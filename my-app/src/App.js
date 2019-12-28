5e85f6de168984c02433f179e65c4d864974d0ad
gitMarcldo <marcldo@users.noreply.github.com>
2019-12-28 11:14:30 -0500

    header footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,28 +4,48 @@ import "./App.css";
 import team from "./team.json";
 
 class App extends Component {
-  // Setting this.state.team to the team json array
+
   state = {
     team
   };
 
-  // name; title; desc; image
-
-  // Map over this.state.team and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="wrapper">
-        {this.state.team.map(member => (
-          <Card
-            id={member.id}
-            key={member.id}
-            name={member.name}
-            image={member.image}
-            occupation={member.occupation}
-            desc={member.desc}
-          />
-        ))}
-      </div>
+      <>
+        <nav>
+          <div className="logo-container">
+            <img alt="acuity logo" src="https://www.acuityads.com/wp-content/themes/acuity-theme/imgs/acuity_logo.svg"></img>
+          </div>
+        </nav>
+        <div className="team-container">
+          <div className="overview">
+            <h3> Our Team</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium quia autem repellat optio quo nulla, mollitia non, magni explicabo itaque,
+              laudantium delectus! Enim, assumenda voluptates! Dolor iste tempora est dicta nisi, architecto assumenda
+              libero consectetur perspiciatis eveniet vel maiores autem. Accusamus, eveniet consectetur minima ipsum
+            dicta nobis provident nisi nostrum.</p>
+          </div>
+
+          <div className="wrapper">
+            {this.state.team.map(member => (
+              <Card
+                id={member.id}
+                key={member.id}
+                name={member.name}
+                image={member.image}
+                occupation={member.occupation}
+                desc={member.desc}
+              />
+            ))}
+          </div>
+        </div>
+        <footer>
+          <div className="footer-text">
+            <p>© 2019 AcuityAds. All rights reserved.</p>
+          </div>
+        </footer>
+      </>
     );
   }
 }
