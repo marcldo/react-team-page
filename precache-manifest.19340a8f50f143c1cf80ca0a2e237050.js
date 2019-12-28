cab65afa11c9aa086135a1eff5d5545b709704da
gitMarcldo <marcldo@users.noreply.github.com>
2019-12-28 11:40:12 -0500

    Updates

---
+++ precache-manifest.19340a8f50f143c1cf80ca0a2e237050.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2da3cd4fbad1a31a2f61e4d79b3eec6",
+    "url": "/react-team-page/index.html"
+  },
+  {
+    "revision": "34ebfddd5ac1e015abb5",
+    "url": "/react-team-page/static/css/main.f13de2cb.chunk.css"
+  },
+  {
+    "revision": "e1ce69ecf5119febe4a1",
+    "url": "/react-team-page/static/js/2.2e98ccc5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-team-page/static/js/2.2e98ccc5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34ebfddd5ac1e015abb5",
+    "url": "/react-team-page/static/js/main.8d5d1a7c.chunk.js"
+  },
+  {
+    "revision": "8471dc4a13af60c17462",
+    "url": "/react-team-page/static/js/runtime-main.ebff971f.js"
+  }
+]);
